d3bdb9303929c6398ae05781311ce30eaa0b97d9
Jaimen Lathia <5757596+Jmen@users.noreply.github.com>
2025-06-07 15:05:49 +0100

    feat: openapi example endpoint

---
+++ app/api/openapi/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+import { document } from '../openapi';
+
+export async function GET() {
+  return NextResponse.json(document, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+} 
